0d037bed50d234e5799ac7ef490fac7cae4084fa
Tomas Perez <tomasitopg6@gmail.com>
2020-06-18 23:44:42 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,7 +7,7 @@ def my_all?(collection)
   block_return_values = []
   
   while i < collection.length do
-    yield(collection[i])
+    block_return_values << yield(collection[i])
     i = i + 1
   end
 
